a719b1d3d1c87c200329ba0f0b7a45087a6e5667
milicasvonja <37382220+milicasvonja@users.noreply.github.com>
2024-01-21 00:33:08 +0100

    Adding id in Card.js

--- src/Card.js
+++ src/Card.js
@@ -5,6 +5,7 @@ const Card = ({ id, name, username, email }) => {
         <div className="bg-light-green dib br3 pa3 ma2 grow tc">
             <img alt="Robots" src={`https://robohash.org/${username}?200x200`}></img>
             <div>
+                <h3>{id}</h3>
                 <h2>{name}</h2>
                 <p>{email}</p>
             </div>
