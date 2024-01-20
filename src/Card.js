140e52718457e3b77124a4ac52589774d0eb8ca3
milicasvonja <37382220+milicasvonja@users.noreply.github.com>
2024-01-20 23:54:22 +0100

    Add Card with robots and import tachyons

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div className="bg-light-green dib br3 pa3 ma2 grow">
+            <img alt="Robots" src="https://robohash.org/milicasvonja"></img>
+            <div>
+                <h2>Milica Svonja</h2>
+                <p>milica@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
